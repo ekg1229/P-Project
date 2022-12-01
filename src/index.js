544a68966d5f5f3d51a76fa81e8bb174d7246883
ekg1229 <ekg1229@naver.com>
2022-12-01 23:16:43 +0900

    modify index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //import user pages
 import SignIn from './pages/user/SignIn'; //로그인 페이지
 import SignUp from './pages/user/SignUp'; //회원가입 페이지
-import ForgotInfo from './pages/user/ForgotInfo'; //아이디/비밀번호 찾기 페이지
+import ForgotPw from './pages/user/ForgotPw'; //아이디/비밀번호 찾기 페이지
 import EditInfo from './pages/user/EditInfo'; //개인정보 수정 페이지
 import CheckInfo from './pages/user/CheckInfo'; //개인정보 확인 페이지
 
@@ -24,13 +24,13 @@ root.render(
       <Route path="/" element={<SignIn/>}/>
       <Route path="signin" element={<SignIn/>}/>
       <Route path="signup" element={<SignUp/>}/>
-      <Route path="forgotinfo" element={<ForgotInfo/>}/>
+      <Route path="forgotpw" element={<ForgotPw/>}/>
       <Route path="editinfo" element={<EditInfo/>}/>
       <Route path="checkinfo" element={<CheckInfo/>}/>
 
       <Route path="main" element={<Main/>}/>
       <Route path="general" element={<General/>}/>
-      <Route path="accient" element={<Accident/>}/>
+      <Route path="accident" element={<Accident/>}/>
     </Routes>
   </BrowserRouter>
 );
