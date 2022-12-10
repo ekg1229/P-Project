d9a8b0befccc45cfa34c3347f3a608714d34a901
ekg1229 <ekg1229@naver.com>
2022-12-11 00:29:31 +0900

    내 정보 확인 페이지

---
+++ src/pages/user/EditImei.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from 'axios';
+
+function EditImei() {
+  return(
+    <>
+    EditImei
+    </>
+  )
+}
+
+export default EditImei;
--- src/pages/user/UserInfo.jsx
+++ src/pages/user/UserInfo.jsx
@@ -1,36 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import {Container, Nav} from 'react-bootstrap';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import setAuthorizationToken from '../../utils/setAuthorizationToken';
+
+import TabContent from '../../components/TabContent';
 import CheckInfo from './CheckInfo';
 import EditPw from './EditPw';
 import EditImei from './EditImei';
-import "../../styles/user/UserInfo.css";
 
+import "../../styles/user/UserInfo.css";
 import axios from 'axios';
 
 //내 정보 페이지
 function UserInfo() {
-  const [tab, setTab] = useState(0);
+  const [tab, setTab] = useState("0");
 
   return (
     <div className="wrapper">
       <Header/>
       <section className="main_visual">
-      <Container>
-        <Nav className="mt-5 mb-3" variant="tabs" defaultActiveKey="link-0">
-          <Nav.Item>
-            <Nav.Link eventKey="link-0" onClick={()=>{setTab(0)}}>개인정보 수정/확인</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1" onClick={()=>{setTab(1)}}>비밀번호 변경</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2" onClick={()=>{setTab(2)}}>Serial 추가/삭제</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Container>
+        <Container>
+          <Nav className="mt-5 mb-3" variant="tabs" defaultActiveKey="link-0">
+            <Nav.Item>
+              <Nav.Link eventKey="link-0" onClick={()=>{setTab("0")}}>개인정보 수정/확인</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1" onClick={()=>{setTab("1")}}>비밀번호 변경</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2" onClick={()=>{setTab("2")}}>Serial 추가/삭제</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
       </section>
       <Footer/>
     </div>
