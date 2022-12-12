d17770a63431cda37982280fac8661ec9e55df40
ekg1229 <ekg1229@naver.com>
2022-12-12 15:37:50 +0900

    modify Page500.jsx

--- src/pages/error/Page500.jsx
+++ src/pages/error/Page500.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../../styles/error/Page500.css";
 
 function Page500() {
+  const navigate = useNavigate();
+  
+  const timeout = () => {
+    setTimeout(()=>{
+      navigate('/main');
+    }, 10000);
+  };
+
+  useEffect(()=>{
+    timeout();
+    return () => {
+      clearTimeout(timeout);
+    };
+  });
+
   return (
     <>
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"/>
-      <div class="error-header"> </div>
-      <div class="container ">
+      <div class="error-header"></div>
+      <div class="container">
           <section class="error-container text-center">
               <h1>500</h1>
               <div class="error-divider">
-                  <h2>ooops!!</h2>
-                  <p class="description">SOMETHING WENT WRONG.</p>
+                  <h2>서버 에러</h2>
+                  <p class="description">10초 후에 메인 페이지로 이동됩니다.</p>
               </div>
               <a href="/main" class="return-btn"><i class="fa fa-home"></i>Home</a>
           </section>
