e24ae450a3dcfc79297b573b8f05dda2670263a1
ekg1229 <ekg1229@naver.com>
2022-12-12 15:37:41 +0900

    modify Page419.jsx

--- src/pages/error/Page419.jsx
+++ src/pages/error/Page419.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../../styles/error/Page419.css";
 
 function Page419() {
+  const navigate = useNavigate();
+
+  const timeout = () => {
+    setTimeout(()=>{
+      navigate('/main');
+    }, 10000);
+  };
+
+  useEffect(()=>{
+    timeout();
+    return () => {
+      clearTimeout(timeout);
+    };
+  });
+  
   return (
     <>
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"/>
-      <div class="error-header"> </div>
-      <div class="container ">
+      <div class="error-header"></div>
+      <div class="container">
           <section class="error-container text-center">
               <h1>419</h1>
               <div class="error-divider">
-                  <h2>PAGE EXPIRED.</h2>
-                  <p class="description">We Couldn't Find This Page</p>
+                  <h2><b>인증 만료</b></h2>
+                  <p class="description">10초 후에 메인 페이지로 이동됩니다.</p>
               </div>
               <a href="/main" class="return-btn"><i class="fa fa-home"></i>Home</a>
           </section>
