6c790eef9e11debd4a604e4bc500534671aa1497
ekg1229 <ekg1229@naver.com>
2022-12-12 15:37:10 +0900

    modify setProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
 	app.use(
 		createProxyMiddleware('/api', {
-			target: 'http://localhost:8080', 
+			target: 'http://141.164.35.18:8080', 
 			changeOrigin: true,
 		})
 	);
