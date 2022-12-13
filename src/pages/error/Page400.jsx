3f762045d27e41664c5032d0bd503db9d3fb7b0c
ekg1229 <ekg1229@naver.com>
2022-12-13 21:00:54 +0900

    modify Page400.jsx

--- src/pages/error/Page400.jsx
+++ src/pages/error/Page400.jsx
@@ -24,12 +24,12 @@ function Page400() {
       <div class="error-header"></div>
       <div class="container">
           <section class="error-container text-center">
-              <h1>400</h1>
+              <h1>토큰 에러</h1>
               <div class="error-divider">
-                  <h2>토큰 에러</h2>
-                  <p class="description">10초 후에 메인 페이지로 이동됩니다.</p>
+                  <h2>10초 후에 메인 페이지로 이동됩니다.</h2>
+                  {/* <p class="description">10초 후에 메인 페이지로 이동됩니다.</p> */}
               </div>
-              <a href="/main" class="return-btn"><i class="fa fa-home"></i>Home</a>
+              <a href="/main" class="return-btn" style={{fontSize: "2rem"}}><i class="fa fa-home fa-2x"></i>Home</a>
           </section>
       </div>
     </>
