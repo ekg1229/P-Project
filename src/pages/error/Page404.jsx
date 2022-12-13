5e7b30d64ecd4470791cfd11bcb0a0dd717b89e2
ekg1229 <ekg1229@naver.com>
2022-12-13 21:01:04 +0900

    modify Page404.jsx

--- src/pages/error/Page404.jsx
+++ src/pages/error/Page404.jsx
@@ -24,12 +24,12 @@ function Page404() {
       <div class="error-header"></div>
       <div class="container">
           <section class="error-container text-center">
-              <h1>404</h1>
+              <h1>페이지를 찾을 수 없습니다.</h1>
               <div class="error-divider">
-                  <h2>페이지를 찾을 수 없습니다.</h2>
-                  <p class="description">10초 후에 메인 페이지로 이동됩니다.</p>
+                  <h2>10초 후에 메인 페이지로 이동됩니다.</h2>
+                  {/* <p class="description">10초 후에 메인 페이지로 이동됩니다.</p> */}
               </div>
-              <a href="/main" class="return-btn"><i class="fa fa-home"></i>Home</a>
+              <a href="/main" class="return-btn" style={{fontSize: "2rem"}}><i class="fa fa-home fa-2x"></i>Home</a>
           </section>
       </div>
     </>
