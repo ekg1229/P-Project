62953944d6f2c8c4927298d5fe6ecf5fd5c6e3b9
ekg1229 <ekg1229@naver.com>
2022-12-14 13:39:42 +0900

    modify CheckInfo.jsx

--- src/pages/user/CheckInfo.jsx
+++ src/pages/user/CheckInfo.jsx
@@ -39,49 +39,43 @@ function EditInfo() {
     }
   }, []);
 
-  //개인정보 수정 페이지로 이동
-  const moveEditInfo = () => {
-    console.log("sssssssssssss");
-    let tab = "2";
-  }
-
   return (
     <Container>
       <link rel="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"></link>
       <Col xs={12} className="d-flex align-items-center justify-content-center">
         <div className="shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500" style={{opacity: 0.9, border: "1px solid #282c34", background: "#282c34"}}>
           <div className="text-center text-md-center mb-4 mt-md-0">
-            <h3 className="mb-0" style={{color: "white"}}>개인정보 확인 페이지</h3>
+            <h2 className="mb-0" style={{color: "white"}}>개인정보 확인</h2>
           </div>
           <Form className="mt-4">
             {/* 아이디(이메일) 관련 */}
             <Form.Group id="email" className="mb-4">
-              <Form.Label style={{color: "white"}}>이메일</Form.Label>
-              <div className="form-floating mb-3" style={{color: "white"}}>
+              <Form.Label style={{color: "white", fontSize: "1.2rem"}}>이메일</Form.Label>
+              <div className="form-floating mb-3" style={{color: "white", fontSize: "1.2rem"}}>
                 {email}
               </div>
             </Form.Group>
 
             {/* 이름 관련 */}
             <Form.Group id="name" className="mb-4">
-              <Form.Label style={{color: "white"}}>이름</Form.Label>
-              <div className="form-floating" style={{color: "white"}}>
+              <Form.Label style={{color: "white", fontSize: "1.2rem"}}>이름</Form.Label>
+              <div className="form-floating" style={{color: "white", fontSize: "1.2rem"}}>
                 {name}
               </div>
             </Form.Group>
 
             {/* 나이 관련 */}
             <Form.Group id="age" className="mb-4">
-              <Form.Label style={{color: "white"}}>나이</Form.Label>
-              <div className="form-floating" style={{color: "white"}}>
+              <Form.Label style={{color: "white", fontSize: "1.2rem"}}>나이</Form.Label>
+              <div className="form-floating" style={{color: "white", fontSize: "1.2rem"}}>
                 {age}
               </div>
             </Form.Group>
 
             {/* 수정 관련 */}
-            <Button variant="primary" className="w-100" onClick={moveEditInfo}>
+            {/* <Button variant="primary" className="w-100" onClick={moveEditInfo} style={{fontSize: "1.2rem"}}>
               개인정보 수정 페이지로 이동
-            </Button>
+            </Button> */}
           </Form>
         </div>
       </Col>
