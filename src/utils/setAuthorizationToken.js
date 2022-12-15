578da9f1bf40d3d5fd4823591e8a04abf2204eac
ekg1229 <ekg1229@naver.com>
2022-12-15 11:56:14 +0900

    modify setAuthorization.js

--- src/utils/setAuthorizationToken.js
+++ src/utils/setAuthorizationToken.js
@@ -2,22 +2,17 @@ import axios from "axios";
 import logout from "./logout";
 
 export default function setAuthorizationToken(){
-  console.log("Authorization");
   const token = localStorage.getItem('access_token'); //LS에서 access_token 가져옴
   
   if(token) { //token이 존재하면
-    console.log("success");
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   }
   else{ //token 없으면(권한 X)
-    console.log("error");
     delete axios.defaults.headers.common['Authorization'];
     window.location.href = "/";
-    console.log("권한 없음");
   }
-
   axios.post('/api/blockchain/findBlock', {
-    imei: "7f409b1b3211c37499"
+    imei: "fbda537ae7d7796425"
   },
   {
     headers: {
@@ -25,12 +20,8 @@ export default function setAuthorizationToken(){
     }
   })
   .then((res) => {
-    console.log("resSuccess");
-    console.log(res);
   })
   .catch((err) => {
-    console.log("error");
-    console.log(err);
     if (err.response.status === 404){
       logout();
       window.location.href = "/page404";
@@ -44,4 +35,4 @@ export default function setAuthorizationToken(){
       window.location.href = "/page500";
     }
   })
-}
+};
